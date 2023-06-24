3de3cc6c38b61ea46a564ac5f1b2bca96f13e31b
Deji Ajibola <harjhibolar@gmail.com>
2023-06-24 01:49:08 +0100

    chore : add naira symbol

--- handlers/oanda.go
+++ handlers/oanda.go
@@ -17,7 +17,7 @@ func OandaHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	responseMessaage := OandaResponse{
-		Message: fmt.Sprintf("Current GBP/NGN rates: %s\n Time: %s\n", oandaRates.Response[0].AverageBid, *time),
+		Message: fmt.Sprintf("Current GBP/NGN rates: ₦%s\n Time: %s\n", oandaRates.Response[0].AverageBid, *time),
 	}
 	json.NewEncoder(w).Encode(responseMessaage)
 }
